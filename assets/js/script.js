709ae99fae7b6cf925bf31175a96a67ea2fff920
kitrath <crathmel@gmail.com>
2023-01-09 17:16:23 -0600

    Simple quiz class, event listeners, and quiz actions/transitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,10 @@
 // IIFE to protect global scope
 (function (window, document) {
 
-// TODO: Remove
+// TEST
 for (const question of quizJS) {
     console.log(JSON.stringify(question));
 }
-
-// utility funcitons
-function isObjectEmpty(obj) {
-    return (
-        obj &&
-        Object.keys(obj).length === 0 && 
-        typeof obj.constructor === Object
-    );
-}
-
 // Returns a new HTML Element
 // `tagName` is an element tag name as a string,
 //   e.g. 'li', 'div', 'ul', etc.
@@ -35,53 +25,108 @@ function createEl(tagName, ...classNames) {
     
 }   
 
-// Returns a reference to the removed element.
-// `selector` is a selector string
-// `parentElem` is a reference to the parent element of the element to remove.
-function removeEl(selector, parentElem) {
-    const el = document.querySelector(el);
-    return parentElem.removeChild(el);
-}
-
 // Elements we'll need
-const startButton       = document.querySelector("#start-quiz");
+const startButton       = document.querySelector(".start-quiz");
 const nextButton        = document.querySelector("#next");
-const leaderBoardButton = document.querySelector("#leaderboard");
 
 // At the start of the quiz, we'll remove the current card.  We'll
 // keep the reference to it alive and replace it after the quiz
 const cardContainer    = document.querySelector('.container');
-let initialCardRemoved = false;
 
-// Returns a `ul.quiz-answers` HTMLElement
-function getQuizOptions(optionsList) {
+// Quiz class
+class Quiz {
+    constructor(data) {
+        this.data = data;
+        this._index = 0;
+        this.score = 0;
+        this.inProgress = false;
+        this.numCorrect = 0;
+        this.completed = false;
+    }
+
+    getIndex() {
+        return this._index;
+    }
+
+    setIndex(value) {
+        this._index = value;
+    }
+
+    incrementIndex() {
+        if (this._index === this.data.length - 1) {
+            this.setIndex(0);
+        } else {
+            this.setIndex(this._index + 1);
+        }
+    }
+
+    reset() {
+        this._index = 0;
+        this.score = 0;
+        this.inProgress = false;
+        this.numCorrect = 0;
+        this.completed = false;
+    }
+}
+
+// Instantiate a new quiz object 
+const quiz = new Quiz(quizJS);
+
+// Returns <ul> of answer options for each quiz question.
+// Sets event listeners on each <li> in list that transition
+// on click to either next quiz question or final screen.
+function getQuizOptions(quiz) {
+    const currentIndex = quiz.getIndex();
+    const optionsList = quiz.data[currentIndex]['options'];
+
     const ul = createEl('ul', 'quiz-answers');
+
+    // Build <ul>
     for (const option in optionsList) {
         let classList = ['quiz-answer'];
         if (optionsList[option]['correct']) {
-            // TODO: Consider other options for this
             classList.push('correct');
         }
         let li = createEl('li', classList);
         li.textContent = optionsList[option]['text'];
         ul.appendChild(li);
     }
+
     // respond to clicks on answer options
     ul.addEventListener('click', function(event) {
         let target = event.target;
         if (target.matches('.correct')) {
-            target.style.backgroundColor = '#7cbf83';
+            target.style.backgroundColor = "#7cbf83";
+            // increment quiz.numCorrect()
+            quiz.numCorrect++;
+            // Add 10 points to score
+            quiz.score += 10;
+            // TODO: Remove
+            console.log(`Current score: ${quiz.score}`);
         } else {
-            target.style.backgroundColor = '#f47069';
+            target.style.backgroundColor = "#f47069";
+            // TODO: Remove
+            console.log("WRONG");
         }
-        // use setTimeout to delay transition to next question
-    });
+        // Delay before DOM update
+        setTimeout(function() {
+            const currentCard = document.querySelector('.card');
+            quiz.incrementIndex();
+            cardContainer.replaceChild(
+                createQuizQuestion(quiz),
+                currentCard
+            );
+        // 0.8 second delay before transition
+        }, 800);
+    // Set `once` option to only respond to one click per quiz question
+    }, { once: true });   
 
     return ul;
 }
 
 // Returns a `.card` HTMLelement (<div>) containing a quiz question
-function createQuizQuestion(quizQuestion, count) {
+function createQuizQuestion(quiz) {
+    const quizQuestion = quiz.data[quiz.getIndex()];
     // parent (card)
     const cardDiv = createEl('div', 'card');
     cardDiv.setAttribute('data-quiz-id', quizQuestion['id']);
@@ -90,7 +135,7 @@ function createQuizQuestion(quizQuestion, count) {
     const cardHeaderDiv = createEl('div', 'card-header');
     const quizQuestionH2 = createEl('h2', 'quiz-question-number');
 
-    quizQuestionH2.textContent = `Question #${count}`;
+    quizQuestionH2.textContent = `Question #${quiz.getIndex() + 1}`;
     const quizQuestionText = createEl('p', 'quiz-question-text');
     // Need innerHTML for the blank underline <span>s in some questions
     quizQuestionText.innerHTML = quizQuestion['question'];
@@ -100,7 +145,7 @@ function createQuizQuestion(quizQuestion, count) {
 
     // second child
     const cardBody = createEl('div', 'card-body');
-    const quizOptions = getQuizOptions(quizQuestion['options']);
+    const quizOptions = getQuizOptions(quiz);
     cardBody.appendChild(quizOptions);
 
     // append first child to card
@@ -111,28 +156,15 @@ function createQuizQuestion(quizQuestion, count) {
     return cardDiv;
 }
 
-// TODO: Remove - Current stand-in for quiz initialization
-cardContainer.replaceChild(createQuizQuestion(quizJS[0], 1), document.querySelector('.card'));
 
-// TODO: Revise - Quick and dirty quiz object for testing
-const quiz = {
-    _index: 0,
-    // TODO: Remove - inProgress always true for testing
-    inProgress: true,
-    getIndex: function() {
-        return this._index;
-    },
-    _setIndex: function(number) {
-        this._index = number;
-    },
-    incrementIndex: function() {
-        if (this._index === quizJS.length - 1) {
-            this._setIndex(0);    
-        } else {
-            this._setIndex(this._index + 1);
-        }
-    }
-}
+
+
+
+startButton.addEventListener('click', function(event) {
+    // Quiz is now in progress
+    quiz.inProgress = true;
+    cardContainer.replaceChild(createQuizQuestion(quiz), document.querySelector('.card'));
+});
 
 nextButton.addEventListener('click', function(event) {
     // Introspect global quiz object to see where we are
@@ -143,10 +175,11 @@ nextButton.addEventListener('click', function(event) {
         // get current reference to .card section
         const currentCard = document.querySelector('.card');
         quiz.incrementIndex();
-        let currentIndex = quiz.getIndex();
-        cardContainer.replaceChild(createQuizQuestion(quizJS[currentIndex], currentIndex + 1), currentCard); 
+        cardContainer.replaceChild(createQuizQuestion(quiz), currentCard); 
     }
 });
+
+
 
 // END IIFE
 })(window, document);
