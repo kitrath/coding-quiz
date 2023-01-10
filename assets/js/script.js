295fee7952d0a2a28aeb3edf7e99d7d9221df116
kitrath <crathmel@gmail.com>
2023-01-09 22:25:41 -0600

    Add leaderboard.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 // IIFE to protect global scope
 (function (window, document) {
 
-// TEST
-for (const question of quizJS) {
-    console.log(JSON.stringify(question));
-}
 // Returns a new HTML Element
 // `tagName` is an element tag name as a string,
 //   e.g. 'li', 'div', 'ul', etc.
@@ -25,10 +21,13 @@ function createEl(tagName, ...classNames) {
     
 }   
 
+// Global Constants
+const TIME_LEFT = 20;
+
 // Elements we'll need
 const startButton       = document.querySelector(".start-quiz");
 const nextButton        = document.querySelector("#next");
-
+const countdownTimer    = document.querySelector("#countdown");
 // At the start of the quiz, we'll remove the current card.  We'll
 // keep the reference to it alive and replace it after the quiz
 const cardContainer    = document.querySelector('.container');
@@ -42,6 +41,8 @@ class Quiz {
         this.inProgress = false;
         this.numCorrect = 0;
         this.completed = false;
+        this.timeLeft = TIME_LEFT;
+        this.paused = false;
     }
 
     getIndex() {
@@ -60,12 +61,22 @@ class Quiz {
         }
     }
 
+    pause() {
+        this.paused = true;
+    }
+
+    resume() {
+        this.paused = false;
+    }
+
     reset() {
         this._index = 0;
         this.score = 0;
         this.inProgress = false;
         this.numCorrect = 0;
         this.completed = false;
+        this.timeLeft = TIME_LEFT;
+        this.paused = false;
     }
 }
 
@@ -102,22 +113,36 @@ function getQuizOptions(quiz) {
             // Add 10 points to score
             quiz.score += 10;
             // TODO: Remove
-            console.log(`Current score: ${quiz.score}`);
         } else {
             target.style.backgroundColor = "#f47069";
-            // TODO: Remove
-            console.log("WRONG");
+            
+            // stop timer and subtract 3 seconds
+            quiz.pause();
+            quiz.timeLeft = quiz.timeLeft - 3;
+            quiz.resume();
+
         }
+        // pause before delay
+        quiz.pause();
         // Delay before DOM update
         setTimeout(function() {
-            const currentCard = document.querySelector('.card');
-            quiz.incrementIndex();
-            cardContainer.replaceChild(
-                createQuizQuestion(quiz),
-                currentCard
-            );
-        // 0.8 second delay before transition
-        }, 800);
+            // rusume quiz when callback fires
+            quiz.resume();
+
+            // We're at the last quiz question
+            if (quiz.getIndex() === quiz.data.length - 1) {
+                endQuiz(quiz);
+                return;
+            } else {
+                const currentCard = document.querySelector('.card');
+                quiz.incrementIndex();
+                cardContainer.replaceChild(
+                    createQuizQuestion(quiz),
+                    currentCard
+                );
+            }
+        // 0.7 second delay before transition
+        }, 700);
     // Set `once` option to only respond to one click per quiz question
     }, { once: true });   
 
@@ -156,10 +181,140 @@ function createQuizQuestion(quiz) {
     return cardDiv;
 }
 
+function createResults(quiz) {
+    // Create parent
+    const cardDiv = createEl('div', 'card', 'results');
+    const scoreTotal = quiz.score + quiz.timeLeft;
+    const timeLeft = quiz.timeLeft !== 0 ? quiz.timeLeft : 'no';
+    const message = `
+    You completed with quiz with ${timeLeft} seconds left on the clock.
+    You answered ${quiz.numCorrect} out of ${quiz.data.length} questions correctly.
+    Your total points: ${scoreTotal}.
+    `;
+    // First child
+    const messageEl = createEl('p', 'result-message');
+    messageEl.textContent = message;
+    // Append first chidl
+    cardDiv.appendChild(messageEl);
+
+    const formContainer = createEl('div', 'initial-form-container');
+    // createResultsForm
+    const resultsForm = createResultsForm(scoreTotal);
+    formContainer.appendChild(resultsForm);
+    cardDiv.appendChild(formContainer);
+
+    const cancelButton = document.createElement("button");
+    cancelButton.setAttribute("id", "cancel-btn");
+    cancelButton.textContent = "Cancel";
+
+    cardDiv.appendChild(cancelButton);
+
+    cancelButton.addEventListener('click', () => { window.location.reload() });
+
+    return cardDiv;
+}
+
+// Returns a Form Element and adds event handlers
+function createResultsForm(totalScore) {
+    // parent form
+    const resultsForm = document.createElement("form");
+    resultsForm.setAttribute("id", "leaderboard-form");
+    // first child
+    const initialsLabel = document.createElement("label");
+    initialsLabel.setAttribute("for", "initials");
+    initialsLabel.textContent = 'Save your initials ';
+    // second child
+    const initialsInput = document.createElement("input");
+    initialsInput.setAttribute("type", "text");
+    initialsInput.setAttribute("id", "initials");
+    initialsInput.setAttribute("name", "initials");
+    // thrid child
+
+    const initialsSubmit = document.createElement("button");
+    initialsSubmit.setAttribute("id", "save-initials");
+    initialsSubmit.setAttribute("value", "SAVE");
+    initialsSubmit.textContent = "SAVE";
+    // Append label, input, and submit to form element
+    resultsForm.appendChild(initialsLabel);
+    resultsForm.appendChild(initialsInput);
+    resultsForm.appendChild(initialsSubmit);
+    
+    initialsSubmit.addEventListener('click', function(event) {
+        // Don't reload the page
+        event.preventDefault();
+        let currentStorage = window.localStorage.getItem("leaderboard");
+        if (initialsInput.value === '') {
+            alert("Please enter your initials before you save")
+            return;
+        }
+        // append to localStorage
+        if (currentStorage) {
+            let leaderList = JSON.parse(currentStorage);
+            leaderList.push({name: initialsInput.value, score: totalScore});
+            window.localStorage.setItem("leaderboard", JSON.stringify(leaderList));
+            alert("Thanks for saving!");
+            // For now
+            window.location.reload();
+        // create localStorage
+        } else {
+            let leaderList = [];
+            leaderList.push({name: initials.value, score: totalScore});
+            window.localStorage.setItem("leaderboard", JSON.stringify(leaderList));
+            alert("Thanks for saving!");
+            // For now
+            window.location.reload();
+        }
+    });
+
+    return resultsForm;
+}
+
+function endQuiz(quiz) {
+  // This will stop global timer
+    quiz.completed = true;
+    showTimer(false);
+    const currentCard = document.querySelector('.card');
+    // Transition to results page
+    cardContainer.replaceChild(
+        createResults(quiz), 
+        currentCard
+    );
+}
+
+function startTimer() {
+    let timerInterval = setInterval(function() {
+        if (!quiz.paused) {
+            quiz.timeLeft--;
+            countdownTimer.textContent = quiz.timeLeft;
+        }
+        if (quiz.timeLeft === 0 || quiz.completed) {
+            clearInterval(timerInterval);
+            if (!quiz.timeLeft) {
+               endQuiz(quiz); 
+            }
+        }
+    }, 1000);
+}
+
+function showTimer(show) {
+    if (show) {
+        countdownTimer.style.visibility = 'visible';
+        countdownTimer.textContent = quiz.timeLeft;
+    } else {
+        countdownTimer.style.visibility = 'hidden';
+    }
+}
+
 startButton.addEventListener('click', function(event) {
+    quiz.reset();
     // Quiz is now in progress
     quiz.inProgress = true;
-    cardContainer.replaceChild(createQuizQuestion(quiz), document.querySelector('.card'));
+    showTimer(true);
+    startTimer();
+    cardContainer.replaceChild(
+        createQuizQuestion(quiz), 
+        document.querySelector('.card')
+    );
 });
 
 // END IIFE
